6dfc6244ee533a79cd999032f21c0d10a1450ece
umihico <umihico@users.noreply.github.com>
2020-09-29 20:52:36 +0900

    run only there is no contribution today

--- updater.sh
+++ updater.sh
@@ -1,6 +1,9 @@
 set -xv
-sh umihi.co.sh
-sh books.sh
-sh PortfolioHub.sh
-sh laravel-demo.sh
-sh laravel-template.sh
+curl https://github.com/users/umihico/contributions | grep data-date=\"$(date +'%Y-%m-%d')\" | grep data-count=\"0\"
+if [ $? -eq 0 ]; then
+  sh umihi.co.sh
+  sh books.sh
+  sh PortfolioHub.sh
+  sh laravel-demo.sh
+  sh laravel-template.sh
+fi
